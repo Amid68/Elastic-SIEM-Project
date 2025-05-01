e4cc900fe23e3bd8fc58855e1df85d6e0f7e1f31
Amid Othman <othman.ameed@gmail.com>
2025-05-01 19:04:05 +0300

    Remove visualization folder

---
+++ generate_certs.sh
@@ -0,0 +1,9 @@
+mkdir -p docker/elasticsearch/certs
+cd docker/elasticsearch/certs
+
+# Generate CA
+openssl req -x509 -nodes -newkey rsa:2048 -keyout ca.key -out ca.crt -days 365 -subj "/CN=Elastic-CA"
+
+# Generate server certificate
+openssl req -nodes -newkey rsa:2048 -keyout node.key -out node.csr -subj "/CN=elasticsearch"
+openssl x509 -req -in node.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out node.crt -days 365
